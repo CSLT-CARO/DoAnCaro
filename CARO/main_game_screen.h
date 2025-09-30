33e3abe8c781b643d7df38dcc7a260981ddde562
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2025-09-30 09:20:12 +0700

    Add winner checker mechanism

--- CARO/main_game_screen.h
+++ CARO/main_game_screen.h
@@ -16,6 +16,18 @@ void initGame(GameState& game_state) {
 void printCurrentGame(GameState& game_state) {
 	printBoard(game_state.board);
 
+	PlayerMark winner = checkWinner(game_state.board);
+
+	if (winner == X) {
+		std::cout << "X Won!!!!\n";
+		exit(0);
+	}
+	
+	if (winner == O) {
+		std::cout << "O Won!!!!\n";
+		exit(0);
+	}
+
 	if (not isMoveLeft(game_state.board)) {
 		std::cout << "No available cells left!\n";
 		exit(0);
@@ -23,7 +35,7 @@ void printCurrentGame(GameState& game_state) {
 }
 
 void inputCurrentGame(GameState& game_state) {
-	if (game_state.bot_marker == game_state.whose_turn) {
+	if (game_state.mode == PVE and game_state.bot_marker == game_state.whose_turn) {
 		botPlay(game_state);
 		alternateTurn(game_state.whose_turn);
 		return;
