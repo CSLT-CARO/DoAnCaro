1d7560539daf1a3f349e6d1e4dba7e41ff7dcca8
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2025-09-30 16:04:58 +0700

    Relocate files and add partial support for 12x12 board

--- CARO/CARO.cpp
+++ CARO/CARO.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <stdlib.h>
 #include "start_screen.h"
 #include "choose_mode_screen.h"
 #include "choose_difficulty_screen.h"
+#include "choose_board_screen.h"
 #include "main_game_screen.h"
+#include "play_again_screen.h"
 
 int main() {
     GameState game_state {};
     game_state.current_state = StartScreen;
     
     while (true) {
+        system("cls");
         switch (game_state.current_state) {
             case StartScreen:
                 std::cout << '\n';
@@ -22,9 +26,17 @@ int main() {
                 std::cout << '\n';
                 processChooseDifficulty(game_state);
                 break;
+            case ChooseBoardType:
+                std::cout << '\n';
+                processChooseBoardType(game_state);
+                break;
             case MainGame:
                 std::cout << '\n';
                 processCurrentGame(game_state);
+                break;
+            case PlayAgain:
+                std::cout << '\n';
+                processPlayAgainScreen(game_state);
                 break;
             case Exit:
                 std::cout << "exited";
